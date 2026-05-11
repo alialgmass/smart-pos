c5a59260020cf5125b0bb3be71108244de0fc1f1
ali <eali7563@gmail.com>
2026-05-11 03:49:37 +0300

    init

--- Modules/Offline/routes/api.php
+++ Modules/Offline/routes/api.php
@@ -5,6 +5,6 @@ use Modules\Offline\Http\Controllers\OfflineSyncController;
 use Modules\Offline\Http\Controllers\SyncDataController;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::post('/offline/sync', OfflineSyncController::class)->name('offline.sync');
-    Route::get('/offline/sync-data', SyncDataController::class)->name('offline.sync-data');
+    Route::post('/offline/sync', [OfflineSyncController::class, '__invoke'])->name('offline.sync');
+    Route::get('/offline/sync-data', [SyncDataController::class, '__invoke'])->name('offline.sync-data');
 });
