7d7f9d520654dcd32e9ff2dc22b9f6ba032da86f
ali <eali7563@gmail.com>
2026-05-11 03:48:34 +0300

    init

--- Modules/Billing/routes/api.php
+++ Modules/Billing/routes/api.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Billing\Http\Controllers\WebhookController;
 
-Route::post('/billing/webhook', WebhookController::class)->name('billing.webhook');
+Route::post('/billing/webhook', [WebhookController::class, '__invoke'])->name('billing.webhook');
